77196812ad81cfdf20f802c1e98d7ab0dfb848e3
Pim Pieters <pim@pieters.tv>
2025-10-12 21:21:27 +0200

    ricochet: removed

--- ricochet/components.cpp
+++
@@ -1,5 +0,0 @@
-#include "components.hpp"
-
-b2Vec2 body_s::center() const {
-    return b2Body_GetPosition(b2_id);
-}
--- ricochet/entities.hpp
+++
@@ -1,9 +0,0 @@
-#include "components.hpp"
-
-void remove_entity(ecs_s* ecs, entity_id entity);
-
-entity_id create_world(ecs_s* ecs);
-entity_id create_player(ecs_s* ecs, float x, float y);
-entity_id create_wall(ecs_s* ecs, float x, float y, float w, float h);
-entity_id create_bullet(ecs_s* ecs, float x, float y, float dx, float dy);
-entity_id create_enemy(ecs_s* ecs, float x, float y);
